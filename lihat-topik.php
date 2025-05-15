99411e8d03e7f7d2c7aeeb1c1c636ef4014ad9a5
alutfiyanuar <alutfiyanuar@gmail.com>
2025-05-15 12:39:28 +0700

    setting volume

---
+++ lihat-topik.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT"
+      crossorigin="anonymous"
+    />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Handlee&family=Italianno&family=Sacramento&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap"
+      rel="stylesheet"
+    />
+  <title>title</title>
+</head>
+<body>
+  <?php include 'menu.php;'?>
+  <div class="container">
+    <?php
+
+  ?>
+  </div>
+  
+</body>
+</html>
